cc9fe59cd0f92396135de9008f04253b9929b0b3
Tatyana25081974 <tatyanakorabelnikovamama@gmail.com>
2025-07-23 15:35:59 +0300

    fix

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
 
 const Logo = () => {
   return (
-    <img
-      src="/header-logo.svg"
-      alt="Rental Car Logo"
-      width={104}
-      height={16}
-      className={styles.logo}
-    />
+    <Link to="/">
+      <img
+        src="/header-logo.svg"
+        alt="Rental Car Logo"
+        width={104}
+        height={16}
+        className={styles.logo}
+        style={{ cursor: "pointer" }}
+      />
+    </Link>
   );
 };
 
